624f0a7ab59a94cc72b74e0e69d4cc4f5ff40b4d
stefaniarincu <stefania.rincu@gmail.com>
2024-04-11 16:56:15 +0300

    added modifiers

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.18;
 
 contract Voting {
-    enum VotingState { Started, Ended } 
+    enum VotingState { NotStarted, Started, Ended } 
 
     struct Candidate {
-        string name;
-        uint256 numVotes;
         address candidateAddress;
+        string name;
+        string description;
+        uint256 numVotes;   
     }
 
     struct Voter {
         mapping(uint256 => bool) hasVoted;        
-        uint candidateId;
         uint256 gasSpent;
     }
 
@@ -22,8 +22,16 @@ contract Voting {
     Candidate[] public candidateList;
     VotingState public currentState;
 
-    event SomeoneVoted(address indexed voter, uint indexed candidateId);
-    event SomeoneCandidated(address indexed candidate, string name);
+    event SomeoneCandidated(address indexed candidateAddress, string name);
+    event SomeoneVoted(address indexed voter, uint256 indexed candidateId);
+
+    event StartVote();
+    event EndVote();
+
+    modifier onlyIfVotingNotStarted {
+        require(currentState == VotingState.NotStarted, "Voting has already started or ended!");
+        _;
+    }
 
     modifier onlyIfVotingStarted {
         require(currentState == VotingState.Started, "Voting has not started!");
@@ -40,5 +48,18 @@ contract Voting {
         _;
     }
 
-    
+    function startVoting() public onlyIfVotingNotStarted onlyAdmin {
+        currentState = VotingState.Started;
+        emit StartVote();
+    }
+
+    function endVoting() public onlyIfVotingStarted onlyAdmin {
+        currentState = VotingState.Ended;
+        emit EndVote();
+    }
+
+    function addCandidate(string memory _name, string memory _description) public onlyIfVotingNotStarted {
+        candidateList.push(Candidate(msg.sender, _name, _description, 0));
+		emit SomeoneCandidated(msg.sender, _name);
+    }
 }
